9693d3f9d45538a027c9482d3f0dd7782919f934
Saad Farhan <56558687+saadfarhan124@users.noreply.github.com>
2024-03-02 01:59:14 +0500

    fixed landing text decoration

--- src/app/sections/landing.tsx
+++ src/app/sections/landing.tsx
@@ -12,7 +12,7 @@ const Landing = () => {
             />
             <div className="absolute bottom-0 left-0 right-0 text-center">
                 <p className="text-2xl md:text-4xl lg:text-6xl xl:text-6xl mb-2 font-extrabold text-white">Edit. Animate. Create.</p>
-                <div className="w-2/3 border-b border-red mx-auto" style={{ borderWidth: 2 }} />
+                <div className="w-72 md:w-3/6 xl:w-3/6 2xl:w-3/12 border-b border-red mx-auto" style={{ borderWidth: 2 }} />
             </div>
 
         </div>
