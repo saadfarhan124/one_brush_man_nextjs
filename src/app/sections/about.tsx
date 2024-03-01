218ab12fe85bea6b9a7f24ff059458511290c538
Saad Farhan <56558687+saadfarhan124@users.noreply.github.com>
2024-03-02 02:53:21 +0500

    added red border in about

--- src/app/sections/about.tsx
+++ src/app/sections/about.tsx
@@ -7,8 +7,10 @@ const About = () => {
     return (
         <div id="about" style={styles.centerContent} className="pt-10 lg:pt-20 xl:pt-20">
             <div className="pt-20">
-                <p className="text-center md:text-left text-lg md:text-2xl lg:text-3xl xl:text-3xl mb-2 font-extrabold" style={{}}>Saad Irfan</p>
-                <p className="text-center md:text-left text-sm md:text-lg lg:text-xl xl:text-xl mb-2 font-normal	">Designer / Concept Artist</p>
+                <p className="text-center md:text-left text-lg md:text-2xl lg:text-3xl xl:text-3xl mb-2 font-extrabold">Saad Irfan</p>
+                <div className="mx-auto md:mx-0 w-28 md:w-32 lg:w-40 border-b border-red" style={{ borderWidth: 2}} />
+
+                <p className="pt-4 text-center md:text-left text-sm md:text-lg lg:text-xl xl:text-xl mb-2 font-normal	">Designer / Concept Artist</p>
                 <p className="text-center md:text-left text-base font-normal	leading-6	" style={{ color: 'rgba(255, 255, 255, 0.40)' }}>As an artist with a background in traditional, abstract, and concept art, as well as theater,
                     I have a diverse range of experiences that inform my work. My skillset encompasses traditional and digital art, animation,
                     and UI/UX engineering, using tools such as Figma, Adobe Creative Suite, and Blender.
