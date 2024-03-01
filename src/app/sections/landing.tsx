c07d51a8bae30c937ff7a25adb53b02ef9f5b72a
Saad Farhan <56558687+saadfarhan124@users.noreply.github.com>
2024-03-02 02:03:15 +0500

    added gradient to the landing page

--- src/app/sections/landing.tsx
+++ src/app/sections/landing.tsx
@@ -10,6 +10,8 @@ const Landing = () => {
                 layout="fill"
                 objectFit="cover"
             />
+            <div className="absolute inset-x-0 bottom-0 h-20 bg-gradient-to-b from-transparent to-black"></div>
+
             <div className="absolute bottom-0 left-0 right-0 text-center">
                 <p className="text-2xl md:text-4xl lg:text-6xl xl:text-6xl mb-2 font-extrabold text-white">Edit. Animate. Create.</p>
                 <div className="w-72 md:w-3/6 xl:w-3/6 2xl:w-3/12 border-b border-red mx-auto" style={{ borderWidth: 2 }} />
