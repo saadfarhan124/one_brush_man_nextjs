e8326c4799e992bcb76e9942c842a277bff9bb5a
Saad Farhan <56558687+saadfarhan124@users.noreply.github.com>
2024-05-21 20:45:01 +0500

    removed deadcode added meta tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,6 @@ import Contact from "./sections/contact";
 import Footer from "./sections/footer";
 
 export default function Home() {
-  const styles = {
-    landingImage: {
-      backgroundImage: "url('/main_bg.png')", backgroundSize: "cover", backgroundPosition: "center"
-    },
-    portfolioFooterBg: {
-      backgroundColor: "rgba(24, 24, 24, 0.71)", backdropFilter: "blur(5px)",
-    },
-    portfolioHeading: {
-      display: "inline-block",
-      borderBottom: "4px solid #DA0037"
-    },
-
-    centerContent: {
-      paddingLeft: "25%", paddingRight: "25%"
-    }
-  }
-
   return (
     <main>
       <Navbar />
@@ -38,7 +21,3 @@ export default function Home() {
     </main>
   );
 }
-
-
-
-// filter: invert(13%) sepia(97%) saturate(5159%) hue-rotate(338deg) brightness(83%) contrast(109%)
